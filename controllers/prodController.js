b0e010dc51e8b85e394c9cbf9473a9ac243d1f6d
Anurag <anuragcpyadav031@gmail.com>
2026-01-23 11:08:21 +0530

    fix: vercel deployment

--- controllers/prodController.js
+++ controllers/prodController.js
@@ -1,5 +1,5 @@
 const Product = require('../models/productModel');
-const apiFeatures = require('../utils/apiFeatures');
+const apiFeatures = require('../utils/APIFeatures');
 
 exports.getAllProducts = async (req, res) => {
     try {
